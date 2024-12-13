d8e80fa723ea387df62c56b07260590166ee999f
Amirmohamadsorani <amirmohamads1387@gmail.com>
2024-12-13 07:07:04 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>moket_foroshi</title>
+    <link rel="stylesheet" href="moket.css">
+</head>
+<body>
+   <header class="header">  <span>فروشگاه موکت</span> </header> 
+   <div class="nav">
+   <div  class="nav_text"> <a href="login.php"> ورود به سایت</a></div>
+         <div  class="nav_text"> <a href="register.php">عضویت در سایت</a></div>
+         <div  class="nav_text"><a href="nemonekar.php">نمونه کار</a></div>
+         <div  class="nav_text"><a href="price_table.php">لیست قیمت</a></div>
+         <div  class="nav_text"><a href="moarefi_anvae_moket.php">معرفی انواع موکت</a></div>
+   </div>
+<main class="main"> 
+   <section class="main_left"> 
+   <P class="text_section">موکت ها از جمله کفپوش های محبوبی هستند که اغلب مواقع در خانه ها، بخصوص اتاق خواب، دفتر کار خانگی، اتاق بازی کودکان و پلکان مورد استفاده قرار می گیرند. 
+      ممکن است اینطور به نظر برسد که موکت ها فقط مصرف خانگی داشته باشند؛ اما در بسیاری از محیط های اداری و همچنین هتل ها نیز موکت ها کاربردهای بسیاری دارند.
+       بنابراین شناخت انواع موکت و تشخیص اینکه کدام جنس موکت می تواند برای شما مناسب باشد پیش از انتخاب طرح ها و رنگ های مختلف موکت اولویت دارد.</P>
+   </section>
+   <aside class="main_right">
+   <div>
+      <div>
+      <img class="picture" src="picture/moket3.jpg" alt="">
+      </div>
+      <div>
+      <img class="picture" src="picture/moket4.jpg" alt="">
+      </div>
+   </div>
+   <div>
+      <div>
+         <img class="picture" src="picture/Barber-carpet-3.jpg" alt="">
+      </div>
+      <div>
+         <img class="picture" src="picture/moket5.jpg" alt="">
+      </div>
+   </div>
+   <div>
+      <div>
+ <img class="picture" src="picture/moket6.jpg" alt="">
+      </div>
+      <div>
+<img class="picture" src="picture/moket7.jpg" alt="">
+      </div>
+   </div>
+
+     </aside>
+</main>
+   <footer id="footer">
+     <div class="tex"> 
+      <span>شماره تماس : 09139319488</span>
+    </div>
+    <div class="tex"><span class="tex">نظر خود را با ما به اشتراک بگذارید:</span></div>
+    <div class="textarea">
+      <form action="" method="post">
+         <textarea class="textarea" name="message" placeholder="دیدگاه خود را بنویسید"></textarea>
+         <input class="value" type="submit" value="ارسال">
+      </form>
+    </div>
+   </footer>
+</body>
+</html>
---
+++ nemonekar.php
@@ -0,0 +1,42 @@
+<?php
+include("tag_a.html")
+?>
+    <div>    <img class="imd" src="picture/nemonekare1.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare2.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare3.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare4.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare5.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare7.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare8.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare9.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare10.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare10.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare11.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare13.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare14.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare15.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare16.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare17.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare18.jpg" alt=""></div>
+    <div>    <img class="imd" src="picture/nemonekare19.jpg" alt=""></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include("footer2.html")
+?>
